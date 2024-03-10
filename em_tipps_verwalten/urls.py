9b02f16342e70d6ab51a4b62dd477fc5505282b2
andreasrudolfwolf@gmail.com <andreasrudolfwolf@gmail.com>
2024-03-10 19:54:48 +0100

    Seiten hinzugefügt

---
+++ em_tipps_verwalten/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.startSeite, name='startSeite'),
+    path('paarungen/', views.paarungen, name='paarungen'),
+    path('tippen/', views.tippen, name='tippen'),
+    #path('loginSeite/', views.loginSeite, name='loginSeite')
+]
--- em_tipps_verwalten/views.py
+++ em_tipps_verwalten/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+#from django.contrib import messages
+#from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+
+def startSeite(request):
+    return render(request, 'startSeite.html')
+
+def paarungen(request):
+    return render(request, 'paarungen.html')
+
+def tippen(request):
+    return render(request, 'tippen.html')
+
+"""def loginSeite(request):
+    #if request.method == 'POST':
+     #   benutzername = request.POST['benutzername']
+      #  passwort = request.POST['passwort']
+        #benutzer = authenticate(request, username=benutzername, password=passwort)
+
+        #if benutzer is not None:
+        #    login(request, benutzer)
+        #    return redirect('login')
+        #else:
+        #    messages.error(request, 'Benutzername oder Passwort nicht korrekt')
+
+    return render(request, 'em_tipps/login.html')
+"""
